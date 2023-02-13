dd4c303a614d1638cf3e7a953627099d721e0c8c
Kevin Mahoney <rigelorionbeta@gmail.com>
2023-02-12 20:20:06 -0500

    Removed plugins from being tracked.

--- Plugins/CreaturePackPlugin/CreaturePackPlugin/Source/CreaturePackEditor/Public/CreaturePackEditorPCH.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-
-#include "SlateBasics.h"
-
-#include "EditorStyleSet.h"
-#include "Editor/UnrealEd/Classes/Settings/EditorLoadingSavingSettings.h"
--- Plugins/CreaturePlugin/Source/CreaturePlugin/Private/CreatureAnimStateMachine.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "CreatureAnimStateMachine.h"
-#include "CreaturePluginPCH.h"
-#include "CreatureAnimTransition.h"
-#include "EdGraph/EdGraphSchema.h"
-UCreatureAnimStateMachine::UCreatureAnimStateMachine(){
-	//StateMachineGraph = NewObject<UCreatureStateMachineGraph>(UCreatureStateMachineGraph::StaticClass());
-
-}
